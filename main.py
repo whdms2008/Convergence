5bb2c563164d4e6949a3d07c41caa22ae8e32e58
whdms2008 <whdms1107@naver.com>
2021-10-03 13:18:48 +0900

    카메라, lidar 계산 스레드 사용 [불안정]

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import random
-
 import PyLidar3
-import numpy as np
 import pandas as pd
-import time  # Time module
+import time
 import cv2
+import threading
 
 
 def output(array):
@@ -15,15 +13,17 @@ def output(array):
         print(e)
 
 
-
 def position_out(number, in_frame):
+    #print(number)
     check = number.keys()
-    print(check)
     h, w, c = in_frame.shape
     out_frame = in_frame
     for i in range(1, 3):
         out_frame = cv2.line(out_frame, (int(w / 3 * i), 0), (int(w / 3 * i), h), (255, 255, 255), 1)
     out_frame = cv2.line(out_frame, (0, int(h / 2)), (w, int(h / 2)), (255, 255, 255), 1)
+    if len(check) == 0:
+        print("아무것도 없음 반환")
+        return out_frame
     if 1 in check:
         out_frame[0:int(h / 2), 0:int(w / 3)] = 0  # 1번째
     if 2 in check:
@@ -39,84 +39,120 @@ def position_out(number, in_frame):
     return out_frame
 
 
-def position_lidar(csv):
+def position_lidar(gen):
     numbers = {}
-    data = csv[0:89] + csv[269:359]
+    data = gen[0:89] + gen[269:359]
 
     j = 5
     for i in range(0, len(data), 5):
         result = sum(data[i:j])
         #print("검출중", i, "도 ~", j, "도 :", result, data[i:j])
         j += 5
-        if 500 < result <= 2500:
+        if 500 <= result <= 1000:
+            #print("검출중", i, "도 ~", j, "도 :", result, data[i:j])
             if 0 <= i <= 60:
-                #print("1번 영역 검출", result)
+                # print("1번 영역 검출", result)
+                #print("60도 -", result)
                 numbers[1] = result
                 continue
             if 61 <= i <= 120:
-                #print("2번 영역 검출", result)
+                # print("2번 영역 검출", result)
+                #print("120도 -", result)
                 numbers[2] = result
                 continue
             if 121 <= i <= 180:
-                #print("3번 영역 검출", result)
+                # print("3번 영역 검출", result)
+                #print("180도 -", result)
                 numbers[3] = result
                 continue
+    #print(numbers)
     return numbers
 
 
-def videoDetector(vcap):
-    # 카메라의 프레임을 지속적으로 받아오기
-    while True:
-        ret, frame = vcap.read()
-        # 이미지 6등분 분할
+class Lidar:
+    def __init__(self, port):
+        data = PyLidar3.YdLidarX4(port)
+        return data
 
-        frame = cv2.flip(frame, 1)  # 좌우 대칭 변경
 
-        frame = position_out(random.randrange(1, 7), frame)
+class Logical(threading.Thread):
+    def __init__(self):
+        super().__init__()
+    def run(self):
+        global gen
+        global lidar
+        while True:
+            #print("계산 시작")
+            lidar = list(next(gen).values())
+            #print("계산 완료")
 
-        cv2.imshow("VideoFrame", frame)
 
-        # # cvs2.waitKey(1) 1은 밀리세컨으로 키입력값 대기 지연시간이다. ESC로 멈춤
-        if cv2.waitKey(1) == 27:
-            vcap.release()  # 메모리 해제
-            cv2.destroyAllWindows()  # 모든창 제거, 특정 창만듣을 경우 ("VideoFrame")
+class Camera(threading.Thread):
+    def __init__(self, num, width=960, height=540):
+        super().__init__()
+        self.num = num
+        self.width = width
+        self.height = height
+        self.cap = cv2.VideoCapture(self.num, cv2.CAP_DSHOW)
+        self.cap.set(3, self.width)
+        self.cap.set(4, self.height)
+
+    def run(self):
+        global frame
+        global test
+        global img
+        print("스레드 작동")
+        while True:
+            ret, frame = self.cap.read()
+            if test == 0:
+                cv2.imshow("video", frame)
+            else:
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                cv2.imshow("video", img)
+            if cv2.waitKey(1) == 27:
+                self.cap.release()  # 메모리 해제
+                cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    global frame
+    global lidar
+    global test
+    global img
+    global gen
+    test = 0
+    t = Camera(1)
+    # sub thread 생성
+    t.start()
+    b = Logical()
+
+    Obj = PyLidar3.YdLidarX4("COM5")
+    while True:  # PyLidar3s.your_version_of_lidar(port,chunk_size)
+        if Obj.Connect():
+            print("라이다 연결 됨")
+            csv_data = []
+            csv_out = []
+            gen = Obj.StartScanning()
+            #print(next(gen))
+            b.start()
+            t = time.time()  # start time
+            i = 0
+            while (time.time() - t) < 100:  # scan for 30 seconds
+                try:
+                    #lidar = list(next(gen).values())
+                    # #print("이미지 출력")
+                    img = position_out(position_lidar(lidar), frame)
+                    test = 1
+                    # cv2.imwrite('img/data[' + str(i) + '].jpg', frame)
+                    # csv_data.append(lidar)
+                    i += 1
+                except Exception as e:
+                    print("이미지 처리 오류", e)
+            Obj.StopScanning()
+            Obj.Disconnect()
+            output(csv_data)
             break
-
-
-cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
-cap.set(3, 960)
-cap.set(4, 540)
-# videoDetector(cap)
-while True:
-    Obj = PyLidar3.YdLidarX4("COM5")  # PyLidar3s.your_version_of_lidar(port,chunk_size)
-    if Obj.Connect():
-        print("라이다 연결 됨")
-        print("카메라 연결 됨")
-        csv_data = []
-        csv_out = []
-        gen = Obj.StartScanning()
-        t = time.time()  # start time
-        i = 0
-        while (time.time() - t) < 30:  # scan for 30 seconds
-            try:
-                ret, frame = cap.read()
-                lidar = list(next(gen).values())
-                position_data = position_lidar(lidar)
-                frame = position_out(position_data, frame)
-                print("이미지 출력")
-                cv2.imwrite('img/data[' + str(i) + '].jpg', frame)
-                csv_data.append(lidar)
-                time.sleep(0.1)
-                i += 1
-            except Exception as e:
-                print("이미지 처리 오류", e)
-                break
-            cv2.imshow("VideoFrame", frame)
-        Obj.StopScanning()
-        Obj.Disconnect()
-        output(csv_data)
-        break
-    else:
-        print("라이다 연결 실패")
-        Obj.Disconnect()
-        continue
+        else:
+            print("라이다 연결 실패")
+            Obj.Disconnect()
+            continue
