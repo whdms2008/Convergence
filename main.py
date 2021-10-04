94abc33fb1fa1c77da095ca1bc3a4c094176d451
Administrator <whdms1107@naver.com>
2021-10-04 22:44:20 +0900

    yolo 파일 추가

--- main.py
+++ main.py
@@ -3,7 +3,62 @@ import pandas as pd
 import time
 import cv2
 import threading
+import numpy as np
 
+
+class Yolo:
+    def __init__(self):
+        # Load Yolo
+        self.net = cv2.dnn.readNet("yolov3_custom_2_last.weights", "yolov3_custom_2.cfg")
+        classes = []
+        with open("yolo/obj.names", "r") as f:
+            classes = [line.strip() for line in f.readlines()]
+        self.layer_names = self.net.getLayerNames()
+        self.output_layers = [self.layer_names[i[0] - 1] for i in self.net.getUnconnectedOutLayers()]
+        colors = np.random.uniform(0, 255, size=(len(classes), 3))
+    def load(self,image):
+        # Loading image
+        imgs = cv2.imread(image)
+        imgs = cv2.resize(imgs, None, fx=0.4, fy=0.4)
+        self.height, self.width, self.channels = img.shape
+    def detect(self):
+        # Detecting objects
+        blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+        self.net.setInput(blob)
+        outs = self.net.forward(self.output_layers)
+    def show(self):
+        # 정보를 화면에 표시
+        class_ids = []
+        confidences = []
+        boxes = []
+        for out in self.outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.5:
+                    # Object detected
+                    center_x = int(detection[0] * self.width)
+                    center_y = int(detection[1] * self.height)
+                    w = int(detection[2] * self.width)
+                    h = int(detection[3] * self.height)
+                    # 좌표
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+                    boxes.append([x, y, w, h])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+        font = cv2.FONT_HERSHEY_PLAIN
+        for i in range(len(boxes)):
+            if i in indexes:
+                x, y, w, h = boxes[i]
+                label = str(self.classes[class_ids[i]])
+                cv2.rectangle(img, (x, y), (x + w, y + h), (255,0,0), 2)
+                cv2.putText(img, label, (x, y + 30), font, 3, (255,0,0), 3)
+        cv2.imshow("Image", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 def output(array):
     try:
@@ -11,8 +66,6 @@ def output(array):
         df.to_csv('data.csv', index=False, header=False)
     except Exception as e:
         print(e)
-
-
 def position_out(number, in_frame):
     #print(number)
     check = number.keys()
@@ -28,36 +81,26 @@ def position_out(number, in_frame):
         return out_frame
     for x in check:
         if x != 1:
-            cv2.putText(out_frame, str(int(x)), (int(w/lines * (x-1) ),int(h/4)),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+            # cv2.putText(out_frame, str(int(x)), (int(w/lines * (x-1) ),int(h/4)),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
             out_frame[0:int(h / 2), int(w / lines * (x-1)) + 1:int(w / lines * x)] = 0  # 2번째
         else:
             out_frame[0:int(h / 2), 0:int(w / lines)] = 0  # 1번째
-            cv2.putText(out_frame, str(x), (int(w/lines),int(h/4)),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+            # cv2.putText(out_frame, str(x), (int(w/lines),int(h/4)),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
     return out_frame
-
-
 def position_lidar(gen):
     numbers = {}
-    data = gen[0:89] + gen[269:359]
+    data = gen[269:359] + gen[0:89]
 
     j = 5
     for i in range(0, len(data), 5):
         result = sum(data[i:j])
         #print("검출중", i, "도 ~", j, "도 :", result, data[i:j])
         j += 5
-        if result <= 1000:
+        if result <= 2500:
             #print("검출중", i, "도 ~", j, "도 :", result, data[i:j])
             numbers[j/5] = result
     #print(numbers)
     return numbers
-
-
-class Lidar:
-    def __init__(self, port):
-        data = PyLidar3.YdLidarX4(port)
-        return data
-
-
 class Logical(threading.Thread):
     def __init__(self):
         super().__init__()
@@ -68,11 +111,10 @@ class Logical(threading.Thread):
             #print("계산 시작")
             lidar = list(next(gen).values())
             #print("계산 완료")
-
-
 class Camera(threading.Thread):
-    def __init__(self, num, width=960, height=540):
+    def __init__(self, num, yolo,width=960, height=540):
         super().__init__()
+        self.yolo = yolo
         self.num = num
         self.width = width
         self.height = height
@@ -80,7 +122,7 @@ class Camera(threading.Thread):
         self.cap.set(3, self.width)
         self.cap.set(4, self.height)
 
-    def run(self):
+    def run(self,y):
         global frame
         global test
         global img
@@ -90,26 +132,27 @@ class Camera(threading.Thread):
             if test == 0:
                 cv2.imshow("video", frame)
             else:
+                self.yolo.load(img)
+                self.yolo.detect()
+                self.yolo.show()
                 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                img = cv2.flip(img,1)
+                #img = cv2.flip(img,1)
                 cv2.imshow("video", img)
             if cv2.waitKey(1) == 27:
                 self.cap.release()  # 메모리 해제
                 cv2.destroyAllWindows()
-
-
 if __name__ == '__main__':
     global frame
     global lidar
     global test
     global img
     global gen
+    y = Yolo()
     test = 0
-    t = Camera(1)
+    t = Camera(0,y)
     # sub thread 생성
     t.start()
     b = Logical()
-
     Obj = PyLidar3.YdLidarX4("COM5")
     while True:  # PyLidar3s.your_version_of_lidar(port,chunk_size)
         if Obj.Connect():
@@ -117,14 +160,11 @@ if __name__ == '__main__':
             csv_data = []
             csv_out = []
             gen = Obj.StartScanning()
-            #print(next(gen))
             b.start()
             t = time.time()  # start time
             i = 0
             while (time.time() - t) < 100:  # scan for 30 seconds
                 try:
-                    #lidar = list(next(gen).values())
-                    # #print("이미지 출력")
                     img = position_out(position_lidar(lidar), frame)
                     test = 1
                     # cv2.imwrite('img/data[' + str(i) + '].jpg', frame)
